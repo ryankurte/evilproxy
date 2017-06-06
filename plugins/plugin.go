fb9805a7816344f7bbb8324766ff848ee2dbfd80
Ryan Kurte <ryankurte@gmail.com>
2017-06-06 17:23:33 +1000

    Playing with plugin concept

---
+++ plugins/plugin.go
@@ -0,0 +1,18 @@
+/**
+ * Plugins package defines plugins for evilproxy
+ * These should write/rewrite requests and responses for nefarious purposes
+ *
+ * Copyright 2017 Ryan Kurte
+ */
+
+package plugins
+
+// RequestHandler interface implemented by plugins that re-write request information
+type RequestHandler interface {
+	ProcessRequest(req *http.Request) http.Request
+}
+
+// ResponseHandler interface implemented by plugins that re-write response information
+type ResponseHandler interface {
+	ProcessResponse(req *http.Request) http.Request
+}
